3d18f4615c3602f20bbfe6d6e0dbe658096f77b8
EJisAwaken <fenoasy@yahoo.com>
2023-09-22 23:47:56 +0300

    i use Html

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let template = document.querySelectorAll(".template");
+let breakfast = document.querySelectorAll(".breakfast");
+let lunch = document.querySelectorAll(".lunch");
+let shakes = document.querySelectorAll(".shakes");
+let dinner = document.querySelectorAll(".dinner");
+let nav = document.querySelectorAll("#navbarListe li");
 
+let food = [template, breakfast, lunch, shakes, dinner];
+
+nav.forEach(function (value, key) {
+    value.addEventListener("click", function () {
+        template.forEach(function (element) {
+            element.style.display = "none";
+        });
+
+        food[key].forEach(function (element) {
+            element.style.display = "block";
+        });
+    });
+});
